cf15af0fb1558cbaeea047b4cc52509963a8010f
Stephen <stephenshan@berkeley.edu>
2015-12-01 12:23:53 -0800

    added notification service to mobile

---
+++ mobile/src/main/java/palsofpaulos/soundscape/NotifyService.java
@@ -0,0 +1,8 @@
+package palsofpaulos.soundscape;
+
+/**
+ * Created by StephenS on 12/1/2015.
+ */
+public class NotifyService {
+    //will use this service to notify watch of a nearby recording
+}
