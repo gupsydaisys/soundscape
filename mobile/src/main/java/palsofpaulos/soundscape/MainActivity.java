027971568c70c1dbe225548bf11d0d36cc093228
Serena Gupta <serenagupta@airbears2-10-142-165-76.airbears2.1918.berkeley.edu>
2015-11-30 18:46:05 -0800

    Added homescreen for mobile app

--- mobile/src/main/java/palsofpaulos/soundscape/MainActivity.java
+++ mobile/src/main/java/palsofpaulos/soundscape/MainActivity.java
@@ -1,5 +1,6 @@
 package palsofpaulos.soundscape;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,6 +9,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,5 +17,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void showRecordings(View view) {
+        startActivity(new Intent(this, AudioActivity.class));
     }
 }
